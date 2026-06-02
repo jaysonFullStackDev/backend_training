804e8a41c6d9182ee909d3c88581ef6ce44221c5
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-06-02 16:11:05 +0800

    Express.js topic and added .gitignore to the code file

---
+++ 02_express/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const userData = [];
+let nextId = 1;
+
+//Create a new user
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  const newUser = { id: nextId++, name, email };
+  userData.push(newUser);
+  res.status(201).send(userData);
+});
+
+//Read all users
+app.get("/users", (req, res) => {
+  res.status(200).send(userData);
+});
+
+//Read one user by ID
+app.get("/users/:id", (req, res) => {
+  const user = userData.find((u) => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).send("User not found");
+  } else {
+    res.status(200).send(user);
+  }
+});
+
+app.put("/users/:id", (req, res) => {
+  const user = userData.find((u) => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+  const { name, email } = req.body;
+  user.name = name;
+  user.email = email;
+  res.status(200).send(user);
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
